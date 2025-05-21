bac1d65aef880cc3c19d00be9e752b8de13d2711
Ammarrs <ltop62060@gmail.com>
2025-05-21 17:22:36 +0300

    making it dynamic so if you want to add colors it will be happy :)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var btn = document.querySelector("#btn");
 var div = document.querySelector("#colored");
 
-var colors = ["red", "yellow", "green"];
+var colors = ["red", "yellow", "green", "blue", "brown"];
 var i = 0;
 
 btn.onclick = function () {
   console.log("pressed!");
   div.style.backgroundColor = colors[i];
   i++;
-  if (i == 3) {
+  if (i == colors.length) {
     i = 0;
   }
 }
