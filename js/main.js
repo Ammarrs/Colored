00c6caba57e82f6db84074978b954b4fc17a5811
Ammarrs <ltop62060@gmail.com>
2025-05-21 18:02:07 +0300

    making dynamic-adding more color by user

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 var btn = document.querySelector("#btn");
 var div = document.querySelector("#colored");
+var colorInput = document.getElementById("colorInput");
+var AddBtn = document.getElementById("AddBtn");
 
 var colors = ["red", "yellow", "green", "blue", "brown"];
 var i = 0;
+
+AddBtn.onclick = function () {
+  var color = colorInput.value;
+  colors.push(color);
+}
+
+
 
 btn.onclick = function () {
   console.log("pressed!");
